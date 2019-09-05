2480456550ee4ffb8ae1574ec4b86773f4214343
Messiff10 <zhongfang.zhang@kikatech.com>
2019-09-05 11:01:05 +0800

    twitter_hi

--- news/hi_twitter.py
+++ news/hi_twitter.py
@@ -7,7 +7,7 @@ from lxml.doctestcompare import strip
 
 last_twitter_content = []
 # 第一步， 模拟发起请求
-url = "https://twitter.com/search?vertical=news&q=lang%3Ahi&src=typd"
+url = "https://twitter.com/search?vertical=news&q=lang%3A"+sys.argv[1]+"&src=typd"
 print(url)
 response = urlopen(url)
 
@@ -21,14 +21,14 @@ print(tree)
 
 # //*[@id="articleslist-wrapper"]/ul/li/p/text()  content
 # //*[@id="articleslist-wrapper"]/ul/li/a/p  title
-spans = tree.xpath('//*[@id="stream-item-tweet-1169437245469839360"]/div/div[2]/div[2]/p')
-print(type(spans),len(spans))
-print(spans)
+spans = tree.xpath('//*/div/div/div/p')
+print(type(spans))
 for span in spans:
-    print(span.text)
+    print(span,span.text)
     last_twitter_content.append(span.text)
 
 with open(file="/home/zhangzhongfang/python/data/"+sys.argv[1]+"_"+str(sys.argv[2])+"_twitter.txt", mode="w") as f:
     for india_twitter in last_twitter_content:
         f.write(india_twitter)
         f.write("\n")
+    time.sleep(5)
